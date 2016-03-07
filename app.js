3fd1a74e9cb2adadc9511e346ed5eb974265e5e9
Xwatson <xuwusheng@hotmail.com>
2016-03-07 14:18:45 +0800

    重写httpProvider

---
+++ app.js
@@ -0,0 +1,52 @@
+/**
+ * Created by xuwusheng on 15/11/12.
+ */
+
+'use strict';
+define(['angular'], function (angular) {
+    return angular.module('app', [], function ($httpProvider) {
+        // 使用传统的请求头
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+        /**
+         * 将对象序列化以&拼接
+         * @param {Object} obj
+         * @return {String}
+         */
+        var param = function (obj) {
+            var query = '',
+                name, value, fullSubName, subName, subValue, innerObj, i;
+
+            for (name in obj) {
+                value = obj[name];
+
+                if (value instanceof Array) {
+                    for (i = 0; i < value.length; ++i) {
+                        subValue = value[i];
+                        fullSubName = name + '[' + i + ']';
+                        innerObj = {};
+                        innerObj[fullSubName] = subValue;
+                        query += param(innerObj) + '&';
+                    }
+                } else if (value instanceof Object) {
+                    for (subName in value) {
+                        subValue = value[subName];
+                        fullSubName = name + '[' + subName + ']';
+                        innerObj = {};
+                        innerObj[fullSubName] = subValue;
+                        query += param(innerObj) + '&';
+                    }
+                } else if (value !== undefined && value !== null)
+                    query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+            }
+
+            return query.length ? query.substr(0, query.length - 1) : query;
+        };
+
+        // 重写默认请求
+        $httpProvider.defaults.transformRequest = [function (data) {
+            return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+        }];
+    })
+
+});
